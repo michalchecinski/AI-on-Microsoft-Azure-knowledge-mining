a1901576fbc5e559ebae2bb9e8d947c0bfc24c5c
Michał Chęciński <michal.checinski1@gmail.com>
2020-12-06 18:38:43 +0100

    Add base web app

---
+++ src/TechnicalDocuIndexer.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TechnicalDocuIndexer.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
