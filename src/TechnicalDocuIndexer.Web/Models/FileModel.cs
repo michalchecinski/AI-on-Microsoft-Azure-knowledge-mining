3372ba21bbace87654aee9c0aeb947d74eac09b3
Maciej Wasik <maciej.wasik1@gmail.com>
2020-12-08 17:37:00 +0100

    files controller draft with temporary service for upload (#2)
    * files controller draft with temporary service for upload
    
    * removed template of controller

---
+++ src/TechnicalDocuIndexer.Web/Models/FileModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechnicalDocuIndexer.Web.Models
+{
+    public class FileModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string FileType { get; set; }
+        public string Extension { get; set; }
+        public string Description { get; set; }
+        public string UploadedBy { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        public byte[] Data { get; set; }
+
+        public FileModel(string id, string name, string fileType, string extension, string description, string uploadedBy, DateTime? createdOn, byte[] data)
+        {
+            Id = id;
+            Name = name;
+            FileType = fileType;
+            Extension = extension;
+            Description = description;
+            UploadedBy = uploadedBy;
+            CreatedOn = createdOn;
+            Data = data;
+        }
+    }
+}
