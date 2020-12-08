dc0a0ad2c72be00d384eee84f329eb2abe018c3a
wasikm04 <maciej.wasik1@gmail.com>
2020-12-08 17:05:51 +0100

    files controller draft with temporary service for upload

--- src/TechnicalDocuIndexer.Web/Startup.cs
+++ src/TechnicalDocuIndexer.Web/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using TechnicalDocuIndexer.Web.Service;
 
 namespace TechnicalDocuIndexer.Web
 {
@@ -20,13 +21,11 @@ namespace TechnicalDocuIndexer.Web
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IFileHandler, TemporaryHandler>();
             services.AddControllersWithViews();
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -36,7 +35,6 @@ namespace TechnicalDocuIndexer.Web
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
