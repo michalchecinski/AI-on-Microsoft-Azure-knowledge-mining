89e0f226c9a0896cb699fd482d7dcde96baeb19f
wasikm04 <maciej.wasik1@gmail.com>
2020-12-22 14:02:50 +0100

    draft

---
+++ src/TechnicalDocuIndexer.Web/Controllers/DocumentDetailsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechnicalDocuIndexer.Web.Controllers
+{
+    public class DocumentDetailsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/TechnicalDocuIndexer.Web/Service/DocumentService.cs
@@ -0,0 +1,31 @@
+﻿using Azure.Search.Documents.Indexes;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechnicalDocuIndexer.Web.Service.Utils
+{
+    public class DocumentService
+    {
+        private string indexName;
+        private Uri endpoint;
+        private string key;
+
+        public DocumentService(IConfiguration config)
+        {
+            indexName = config.GetSection("IndexName").Value;
+            endpoint = new Uri(config.GetSection("SearchEndpoint").Value);
+            key = config.GetSection("APIKey").Value;
+        }
+
+        public List<String> FetchDocument(string key)
+        {
+            var searchClient = new SearchIndexClient(serviceName, indexName, new SearchCredentials(queryApiKey));
+
+            var document = searchClient.Documents.Get<YourDocument>(id);
+            return document;
+        }
+    }
+}
