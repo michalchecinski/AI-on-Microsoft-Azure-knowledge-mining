e6a4bd214604c0266b60870aef50a8e71100c819
Michal Kolendo (MVM) <MVM@kmd.dk>
2021-01-13 22:27:12 +0100

    markdown and json display

---
+++ src/TechnicalDocuIndexer.Web/Util/FileDisplayHelper.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Html;
+using Westwind.AspNetCore.Markdown;
+
+namespace TechnicalDocuIndexer.Web.Util
+{
+    public static class FileDisplayHelper
+    {
+        public static HtmlString ParseToHtml(string extension, string fileContent)
+        {
+            if (extension.EndsWith(FileExtensions.MARKDOWN))
+            {
+                return Markdown.ParseHtmlString(fileContent);
+            }
+            else if (extension.EndsWith(FileExtensions.JSON))
+            {
+                return new HtmlString($"<pre>{fileContent}</pre>");
+            }
+            else
+            {
+                return new HtmlString(fileContent);
+            }
+        }
+    }
+}
---
+++ src/TechnicalDocuIndexer.Web/Util/FileExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace TechnicalDocuIndexer.Web.Util
+{
+    internal static class FileExtensions
+    {
+        public const string JSON = ".json";
+        public const string MARKDOWN = ".md";
+    }
+}
