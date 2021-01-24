0386b19692abf268d584e71fd4034f185307a1a7
Michał Chęciński <michal.checinski@outlook.com>
2021-01-24 16:18:45 +0100

    Repo cleanup (#19)
    * Refactor and clean repo
    
    * Update azure-pipelines.yml for Azure Pipelines

--- src/TechnicalDocuIndexer.Functions/Model/Video.cs
+++ src/TechnicalDocuIndexer.Functions/Model/Video.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
-namespace BingSearch
+namespace TechnicalDocuIndexer.Functions.Model
 {
     class Video
     {
--- src/TechnicalDocuIndexer.Functions/WebAPISkillContract.cs
+++ src/TechnicalDocuIndexer.Functions/WebAPISkillContract.cs
@@ -3,7 +3,7 @@
 
 using System.Collections.Generic;
 
-namespace AzureCognitiveSearch.PowerSkills.Common
+namespace TechnicalDocuIndexer.Functions
 {
     public class WebApiSkillRequest
     {
